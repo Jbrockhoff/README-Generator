63c88046ace0eb849ef709477ac8f1717b265ce9
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-02-16 10:30:19 -0600

    began questions array in index.js

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  //[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+  //[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC
+  //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
 
+console.log(fs, inquirer)
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer.prompt ([
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of your application?',
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'What is required to run your application locally?',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How will this application be used?',
+    },
+    {
+        name: 'license',
+        type: 'checkbox', 
+        message: 'Choose a License',
+        choices: ["GPL v3", "ISC", "MIT", "MPL 2.0"],
+        
+    },
+    {
+        name: 'contributors',
+        type: 'input',
+        message: 'Please enter any contributors to this application with username and name of the repository:',
+    }, 
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Please describe libraries used for testing software and supply commands:'
+    },
+    {
+        name: 'GitHub',
+        type: 'input',
+        message: 'Please enter your GitHub username',
+    },
+       
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Please enter your email address with contact instructions'
+    },
+])
+.then((response) =>
+response.
+? console.log('Success!')
+: console.log('Unsuccessful')
+);
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+// }
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
+// Title of project
+
+// Sections:
+// Description 
+// Table of contents
+// installation 
+// usage
+// License
+// Contributing
+// tests
+// questions
+
